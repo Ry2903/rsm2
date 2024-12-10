48056370cd431675c2d9830f7440173009eb8b4b
Ryan Reis <ryanbr2903@gmail.com>
2024-12-10 16:34:57 -0300

    Programando a ESP32 com novo código

--- PI_Bluetooth_1/Prog_RSM2_New.ino
+++ PI_Bluetooth_1/Prog_RSM2_New.ino
@@ -1,127 +1,215 @@
-#define NOME_BLUETOOTH "RSM2 TheCar"
+/* Header --------------------------------------------------------------------*/
+/**
+  *****************************************************************************
+  * @file           : PI_Bluetooth.ino
+  * @brief          : Main program body
+  *******************************************************************************
+  * @attention
+  * Projeto integrador da turma 1MTRB (2º semestre de 2024)
+  * Data: 18/11/2024
+  * Realizado por: Maria Eduarda Oliveira, Maria Luiza "Netuno" Rossi, Ryan Reis e Sophia Figueiredo
+  *
+  * Caso não esteja encontrando a placa do ESP32 no Arduino IDE,
+  * ir para Arquivo > Preferências > URLs do Gerenciador de Placas Adicionais
+  * e adicionar a URL https://dl.espressif.com/dl/package_esp32_index.json
+  *
+  * Conexão de hardware:
+  * +--------+-------+
+  * |Ponte H | ESP32 |
+  * +--------+-------+
+  * |EN1 (M1)| D13   | -> não estamos mais utilizando o enable
+  * |EN2 (M2)| D27   | -> não estamos mais utilizando o enable
+  * |IN1 (M1)| D12   |
+  * |IN2 (M1)| D14   |
+  * |IN3 (M2)| D26   |
+  * |IN4 (M2)| D25   |
+  * +----------------+
+  * Caso o motor esteja rodando da direção contrária, 
+  * inverta os pinos que vão para o motor (OUT1 e OUT2 ou OUT3 e OUT4)
+  *
+  * Não se esqueça de trocar o nome do Bluetooth
+  *****************************************************************************
+  */
+
+/* Nome do Bluetooth ---------------------------------------------------------*/
+#define  NOME_BLUETOOTH "RSM2_TheCar" // Alterar conforme o seu carro
+
+/* Inclusões -----------------------------------------------------------------*/
 #include "BluetoothSerial.h"
 
+/* Definições ----------------------------------------------------------------*/
 #define IN1                 12
 #define IN2                 14
 #define IN3                 26
 #define IN4                 25
-const int debounce = 200; // Tempo de debounce em milissegundos
-const unsigned long Timeout = 5000; // Tempo de segurança em milissegundos
-#define TEMPO_MAXIMO_ESPERA 5000
+#define DEBOUNCE            200  // Tempo de debounce em milissegundos
+#define TEMPO_MAXIMO_ESPERA 5000 // Tempo de segurança em milissegundos
+#define PWM_FRENTE         255  // Potência máxima para frente
+#define PWM_LATERAL        180  // Potência reduzida para direita e esquerda
 
-BluetoothSerial BT; // Instância do BluetoothSerial
+/* Instância do BluetoothSerial ----------------------------------------------*/
+BluetoothSerial Bluetooth;
 
-char comando; ///< Guarda o comando recebido por Bluetooth ou Serial
-unsigned long tempo_sem_comando = 0; ///< Guarda o tempo do último comando recebido
+/* Variáveis globais ---------------------------------------------------------*/
+char comando;  ///< Guarda o comando recebido por Bluetooth ou Serial
+unsigned long tempoSemComando = 0;  ///< Guarda o tempo do último comando
 
-const int motorEsquerdoPWM = 27;  // Pino PWM para controle da velocidade do motor esquerdo
-const int motorDireitoPWM = 13;   // Pino PWM para controle da velocidade do motor direito
+/* Protótipos de funções -----------------------------------------------------*/
+/**
+ * @brief Configura os pinos utilizados para controle dos motores.
+ */
+void configurarPinos();
 
-void configurarPinos(); // Configura os pinos utilizados para controle dos motores.
-void configurarBluetooth(); // Configura a comunicação Bluetooth.
-void processarComando(char comando); // Processa o comando recebido.
-void desligarMotores();
-void movimentarMotores(int velMotorEsq, int velMotorDir, int state1, int state2, int state3, int state4);
+/**
+ * @brief Configura a comunicação Bluetooth.
+ */
+void configurarBluetooth();
 
-void setup() {
+/**
+ * @brief Processa o comando recebido.
+ * @param comando Caractere representando o comando.
+ */
+void processarComando(char comando);
+
+/**
+ * @brief Controla os motores com base nos estados fornecidos.
+ * @param state1 Estado do pino IN1.
+ * @param state2 Estado do pino IN2.
+ * @param state3 Estado do pino IN3.
+ * @param state4 Estado do pino IN4.
+ * @param pwm1 Potência do motor 1 (PWM).
+ * @param pwm2 Potência do motor 2 (PWM).
+ */
+void movimentarMotores(int state1, int state2, int state3, int state4, int pwm1, int pwm2);
+
+/* Função principal ----------------------------------------------------------*/
+/**
+ * @brief Função de configuração inicial do ESP32.
+ */
+void setup() 
+{
   configurarPinos();
   configurarBluetooth();
   Serial.println("Fim Setup");
 }
 
-void loop() {
-    // Verifica se há dados disponíveis na conexão Bluetooth
-    if (BT.available()) {
-        comando = BT.read();
-        tempo_sem_comando = millis(); // Atualiza o tempo do último comando recebido
-        delay(debounce); // Aplica o debounce
-        processarComando(comando);
-    }
+/**
+ * @brief Função principal de loop do ESP32.
+ */
+void loop() 
+{
+  // Verifica se há dados disponíveis na conexão Bluetooth
+  if (Bluetooth.available()) 
+  {
+    comando = Bluetooth.read();
+    tempoSemComando = millis(); // Atualiza o tempo do último comando recebido
+    delay(DEBOUNCE);            // Aplica o debounce
+    processarComando(comando);
+  }
 
-    // Verifica se há dados disponíveis na Serial
-    else if (Serial.available()) {
-        comando = Serial.read();
-        tempo_sem_comando = millis(); // Atualiza o tempo do último comando recebido
-        delay(debounce); // Aplica o debounce
-        processarComando(comando);
+  // Verifica se há dados disponíveis na Serial
+  else if (Serial.available()) 
+  {
+    comando = Serial.read();
+    tempoSemComando = millis(); // Atualiza o tempo do último comando recebido
+    delay(DEBOUNCE);            // Aplica o debounce
+    processarComando(comando);
+  } 
 
-        // Verifica se o tempo desde o último comando excedeu o limite de segurança
-        if ((millis() - tempo_sem_comando) > TEMPO_MAXIMO_ESPERA) {
-            desligarMotores();
-            Serial.println("Motores Desligados");
-        }
-    }
+  // Verifica se o tempo desde o último comando excedeu o limite de segurança
+  if ((millis() - tempoSemComando) > TEMPO_MAXIMO_ESPERA) 
+  {
+    movimentarMotores(LOW, LOW, LOW, LOW, 0, 0);
+    Serial.println("Motores Desligados");
+  }
 }
 
-void processarComando(char comando) {
-  switch (comando) {
+/* Funções auxiliares --------------------------------------------------------*/
+
+/**
+ * @brief Configura os pinos usados para controle dos motores.
+ * Define os pinos como saídas e configura os estados iniciais.
+ */
+void configurarPinos()
+{
+  // Pinos definidos como saída
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  // Estados iniciais dos pinos
+  digitalWrite(IN1, LOW); // Habilita M1
+  digitalWrite(IN2, LOW); // Habilita M2
+  digitalWrite(IN3, LOW); // Habilita M1
+  digitalWrite(IN4, LOW); // Habilita M2
+}
+
+/**
+ * @brief Configura a comunicação Bluetooth.
+ * Define o nome do dispositivo e inicializa a comunicação Serial.
+ */
+void configurarBluetooth() 
+{
+  Serial.begin(9600); // Inicializa a serial para debug
+  Bluetooth.begin(NOME_BLUETOOTH); // Inicia o Bluetooth com o nome definido
+}
+
+/**
+ * @brief Processa o comando recebido via Bluetooth ou Serial.
+ * @param comando Caractere representando o comando.
+ */
+void processarComando(char comando)
+{
+  switch (comando) 
+  {
     case 'F': // Frente
-      movimentarMotores(255, 255, HIGH, LOW, HIGH, LOW);
+      movimentarMotores(HIGH, LOW, HIGH, LOW, PWM_FRENTE, PWM_FRENTE);
       Serial.println("Frente");
       break;
+
     case 'R': // Ré
-      movimentarMotores(255, 255, LOW, HIGH, LOW, HIGH);
+      movimentarMotores(LOW, HIGH, LOW, HIGH, PWM_FRENTE, PWM_FRENTE);
       Serial.println("Ré");
       break;
+
     case 'D': // Direita
-      movimentarMotores(255, 0, LOW, LOW, HIGH, LOW);
+      movimentarMotores(LOW, HIGH, HIGH, LOW, PWM_LATERAL, PWM_LATERAL); // Potência reduzida
       Serial.println("Direita");
       break;
+
     case 'E': // Esquerda
-      movimentarMotores(0, 255, HIGH, LOW, LOW, LOW);
+      movimentarMotores(HIGH, LOW, LOW, HIGH, PWM_LATERAL, PWM_LATERAL); // Potência reduzida
       Serial.println("Esquerda");
       break;
+
     case 'P': // Parar
-      desligarMotores();
+      movimentarMotores(LOW, LOW, LOW, LOW, 0, 0);
       Serial.println("Motores Desligados");
       break;
-    case 'Z': // Zerin da Massa
-      movimentarMotores(0, 255, HIGH, LOW, LOW, LOW);
-      Serial.println("Zerin da Massa");
-      break;
-    default:
-      desligarMotores();
+
+    default: // Comando inválido
+      movimentarMotores(LOW, LOW, LOW, LOW, 0, 0);
       Serial.print("Comando inválido recebido: ");
       Serial.println(comando);
       break;
   }
 }
 
-void configurarPinos() {
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
 
-  digitalWrite(IN1, LOW); // Habilita M1
-  digitalWrite(IN2, LOW); // Habilita M2
-  digitalWrite(IN3, LOW); // Habilita M1
-  digitalWrite(IN4, LOW); // Habilita M2
-
-  ledcSetup(0, 8000, 8);
-  ledcSetup(1, 8000, 8);
-
-  ledcAttachPin(motorEsquerdoPWM, 0);
-  ledcAttachPin(motorDireitoPWM, 1);
-}
-
-void configurarBluetooth() {
-  Serial.begin(9600); // Inicializa a serial para debug
-  BT.begin(NOME_BLUETOOTH); // Inicia o Bluetooth com o nome definido
-}
-
-void desligarMotores() {
-  movimentarMotores(0, 0, LOW, LOW, LOW, LOW);
-}
-
-void movimentarMotores(int velMotorEsq, int velMotorDir, int state1, int state2, int state3, int state4) {
-  // Controla os pinos de direção
-  digitalWrite(IN1, state1);
-  digitalWrite(IN2, state2);
+/**
+ * @brief Controla os motores com base nos estados fornecidos.
+ * @param state1 Estado do pino IN1.
+ * @param state2 Estado do pino IN2.
+ * @param state3 Estado do pino IN3.
+ * @param state4 Estado do pino IN4.
+ * @param pwm1 Potência do motor 1 (PWM).
+ * @param pwm2 Potência do motor 2 (PWM).
+ */
+void movimentarMotores(int state1, int state2, int state3, int state4, int pwm1, int pwm2)
+{
+  analogWrite(IN1, pwm1); // Controle de PWM para o motor 1
+  analogWrite(IN2, pwm2); // Controle de PWM para o motor 2
   digitalWrite(IN3, state3);
   digitalWrite(IN4, state4);
-
-  // Controla a velocidade dos motores via PWM
-  ledcWrite(0, velMotorEsq);
-  ledcWrite(1, velMotorDir);
 }
